f0e5283333bcdcca4a32b3a9ba2ffdc9dabd3b93
Tony DiNitto <think41c@gmail.com>
2015-04-18 20:36:16 -0600

    Separated responsibility in methods.

--- grade_school.rb
+++ grade_school.rb
@@ -12,11 +12,14 @@ class School
   def add(name, grade)
     @school[grade] ||= [] 
     @school[grade] << name
+    sort_kids(grade)
+  end
 
+  def sort_kids(grade)
     @school[grade].sort!
     @school = Hash[@school.sort]
   end
-
+  
   def grade(student_grade)
     @school[student_grade] || []
   end
